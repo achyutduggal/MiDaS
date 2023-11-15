01a66bc4567114882f782bf255bf3c50e4accb42
Chris Careaga <chris_careaga@sfu.ca>
2023-11-15 12:09:17 -0800

    add installation reqs to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     license="",
     python_requires=">3.6",
-    install_requires=[]
+    install_requires=[
+        'numpy>=1.23.5',
+        'opencv-python>=4.8.0.76',
+        'torch>=2.0.1',
+        'torchvision>=0.15.2',
+        'timm>=0.6.12'
+    ]
 )
